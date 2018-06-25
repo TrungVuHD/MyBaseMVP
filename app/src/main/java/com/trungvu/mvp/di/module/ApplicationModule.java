4ca12ce883a2f25e9d1843d6602e4715f191a3ca
Trung Vu <trung.vu@splyza.com>
2018-06-25 18:50:19 +0900

    First commit

---
+++ app/src/main/java/com/trungvu/mvp/di/module/ApplicationModule.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright (C) 2017 MINDORKS NEXTGEN PRIVATE LIMITED
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://mindorks.com/license/apache-v2
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package com.trungvu.mvp.di.module;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.trungvu.mvp.BuildConfig;
+import com.trungvu.mvp.R;
+import com.trungvu.mvp.data.AppDataManager;
+import com.trungvu.mvp.data.DataManager;
+import com.trungvu.mvp.data.db.AppDbHelper;
+import com.trungvu.mvp.data.db.DbHelper;
+import com.trungvu.mvp.data.network.ApiHeader;
+import com.trungvu.mvp.data.network.ApiHelper;
+import com.trungvu.mvp.data.network.AppApiHelper;
+import com.trungvu.mvp.data.prefs.AppPreferencesHelper;
+import com.trungvu.mvp.data.prefs.PreferencesHelper;
+import com.trungvu.mvp.di.ApiInfo;
+import com.trungvu.mvp.di.ApplicationContext;
+import com.trungvu.mvp.di.DatabaseInfo;
+import com.trungvu.mvp.di.PreferenceInfo;
+import com.trungvu.mvp.utils.AppConstants;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+
+@Module
+public class ApplicationModule {
+
+    private final Application mApplication;
+
+    public ApplicationModule(Application application) {
+        mApplication = application;
+    }
+
+    @Provides
+    @ApplicationContext
+    Context provideContext() {
+        return mApplication;
+    }
+
+    @Provides
+    Application provideApplication() {
+        return mApplication;
+    }
+
+    @Provides
+    @DatabaseInfo
+    String provideDatabaseName() {
+        return AppConstants.DB_NAME;
+    }
+
+    @Provides
+    @ApiInfo
+    String provideApiKey() {
+        return BuildConfig.API_KEY;
+    }
+
+    @Provides
+    @PreferenceInfo
+    String providePreferenceName() {
+        return AppConstants.PREF_NAME;
+    }
+
+    @Provides
+    @Singleton
+    DataManager provideDataManager(AppDataManager appDataManager) {
+        return appDataManager;
+    }
+
+    @Provides
+    @Singleton
+    DbHelper provideDbHelper(AppDbHelper appDbHelper) {
+        return appDbHelper;
+    }
+
+    @Provides
+    @Singleton
+    PreferencesHelper providePreferencesHelper(AppPreferencesHelper appPreferencesHelper) {
+        return appPreferencesHelper;
+    }
+
+    @Provides
+    @Singleton
+    ApiHelper provideApiHelper(AppApiHelper appApiHelper) {
+        return appApiHelper;
+    }
+
+    @Provides
+    @Singleton
+    ApiHeader.ProtectedApiHeader provideProtectedApiHeader(@ApiInfo String apiKey,
+                                                           PreferencesHelper preferencesHelper) {
+        return new ApiHeader.ProtectedApiHeader(
+                apiKey,
+                preferencesHelper.getCurrentUserId(),
+                preferencesHelper.getAccessToken());
+    }
+
+    @Provides
+    @Singleton
+    CalligraphyConfig provideCalligraphyDefaultConfig() {
+        return new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/source-sans-pro/SourceSansPro-Regular.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build();
+    }
+}
---
+++ app/src/test/java/com/trungvu/trungvu/framework/mvp/utils/rx/TestSchedulerProvider.java
@@ -0,0 +1,31 @@
+package com.trungvu.trungvu.framework.mvp.utils.rx;
+
+import com.trungvu.mvp.utils.rx.SchedulerProvider;
+
+import io.reactivex.Scheduler;
+import io.reactivex.schedulers.TestScheduler;
+
+public class TestSchedulerProvider implements SchedulerProvider {
+
+    private final TestScheduler mTestScheduler;
+
+    public TestSchedulerProvider(TestScheduler testScheduler) {
+        this.mTestScheduler = testScheduler;
+    }
+
+    @Override
+    public Scheduler ui() {
+        return mTestScheduler;
+    }
+
+    @Override
+    public Scheduler computation() {
+        return mTestScheduler;
+    }
+
+    @Override
+    public Scheduler io() {
+        return mTestScheduler;
+    }
+
+}
